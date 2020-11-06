5abd83b60331fe50801aa78ad9e8951151385b02
Jenny Belanger <jbelangr@gmail.com>
2020-11-05 19:08:54 -0500

    Use spinner SVG

--- src/js/Spinner.js
+++ src/js/Spinner.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { ReactComponent as SpinnerIcon } from '../svg/spinner.svg';
 import { usePromiseTracker } from 'react-promise-tracker';
 
 const Spinner = () => {
 	const { promiseInProgress } = usePromiseTracker();
-	return (promiseInProgress ? <div className="spinner">Loading...</div> : null);
+	if (!promiseInProgress) {
+		return null;
+	}
+	return (
+		<div className="spinner">
+			<SpinnerIcon />
+		</div>
+	);
 };
 
 export default Spinner;
