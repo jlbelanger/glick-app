de59a1e58a909702cdf3e0d78248ea852ef9e093
Jenny Belanger <jbelangr@gmail.com>
2020-12-05 20:28:13 -0500

    Add logo

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,5 +1,6 @@
 import API from './JsonApiForm/Helpers/API';
 import Auth from './Auth/Auth';
+import { ReactComponent as Logo } from '../svg/logo.svg';
 import { NavLink } from 'react-router-dom';
 import React from 'react';
 import { toast } from 'react-toastify';
@@ -19,7 +20,7 @@ export default function Header() {
 		<header id="header">
 			<div className="contain" id="header__contain">
 				{Auth.isLoggedIn() && <NavLink activeClassName="nav__link--active" className="nav__link" to="/profile">Profile</NavLink>}
-				<h1>Glick</h1>
+				<h1><Logo height="32" fill="#fff" title="Glick" /></h1>
 				{Auth.isLoggedIn() && <button className="nav__button" onClick={logout} type="button">Logout</button>}
 			</div>
 		</header>
