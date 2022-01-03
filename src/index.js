6cdd7c5afe7314c630fa2fde34649e48704edcc8
Jenny Belanger <jbelangr@gmail.com>
2022-01-03 16:38:28 -0500

    Remove web-vitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './scss/style.scss';
 import App from './js/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 
 function gtag() {
 	window.dataLayer.push(arguments); // eslint-disable-line prefer-rest-params
@@ -20,8 +19,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
