bf0213ad403ef46c018c1f1177fd476cc643826a
Jenny Belanger <jbelangr@gmail.com>
2023-05-06 12:48:38 -0400

    Show logout button on profile only
    To avoid misclicks

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,11 +1,13 @@
 import { Api, FormosaContext } from '@jlbelanger/formosa';
+import { NavLink, useLocation } from 'react-router-dom';
 import React, { useContext } from 'react';
 import Auth from './Utilities/Auth';
 import { ReactComponent as Logo } from '../svg/logo.svg';
-import { NavLink } from 'react-router-dom';
 
 export default function Header() {
 	const { addToast } = useContext(FormosaContext);
+	const location = useLocation();
+
 	const logout = () => {
 		Api.delete('auth/logout')
 			.then(() => {
@@ -25,7 +27,9 @@ export default function Header() {
 			<div className="contain" id="header__contain">
 				{Auth.isLoggedIn() && <NavLink activeClassName="nav__link--active" className="nav__link" to="/profile">Profile</NavLink>}
 				<div id="logo"><Logo height="28" fill="#fff" title={process.env.REACT_APP_TITLE} /></div>
-				{Auth.isLoggedIn() && <button className="nav__button" onClick={logout} type="button">Logout</button>}
+				{Auth.isLoggedIn() && (
+					<button className="nav__button" disabled={location.pathname !== '/profile'} onClick={logout} type="button">Logout</button>
+				)}
 			</div>
 		</header>
 	);
